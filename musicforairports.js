975af419da4425570d9bc4fb97784c7df455f183
Ryan Amarit <ramarit@gmail.com>
2017-08-19 20:48:47 -0700

    added intro.js guides

--- musicforairports.js
+++ musicforairports.js
@@ -127,7 +127,27 @@ function playSample(instrument, note, destination, delaySeconds = 0) {
     bufferSource.playbackRate.value = playbackRate;
 
     bufferSource.connect(destination);
-    bufferSource.start(audioContext.currentTime + delaySeconds);
+    //bufferSource.start(audioContext.currentTime + delaySeconds);
+
+    var audio = new Audio();
+    audio.src = bufferSource.start(audioContext.currentTime + delaySeconds);
+    audio.controls = true;
+    audio.loop = true;
+    audio.autoplay = true;
+
+    // Establish all variables that your Analyser will use
+    var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+
+    document.getElementById('audio_box').appendChild(audio);
+    context = new webkitAudioContext(); // AudioContext object instance
+    analyser = context.createAnalyser(); // AnalyserNode method
+    canvas = document.getElementById('analyser_render');
+    ctx = canvas.getContext('2d');
+    // Re-route audio playback into the processing graph of the AudioContext
+    source = context.createMediaElementSource(audio); 
+    source.connect(analyser);
+    analyser.connect(context.destination);
+    frameLooper();
   });
 }
 
@@ -193,7 +213,22 @@ fetchSample('AirportTerminal.wav').then(convolverBuffer => {
   
 });
 
-$("a.my-tool-tip").tooltip();
+/*function frameLooper(){
+  window.webkitRequestAnimationFrame(frameLooper);
+  fbc_array = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(fbc_array);
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+  ctx.fillStyle = '#00CCFF'; // Color of the bars
+  bars = 100;
+  for (var i = 0; i < bars; i++) {
+    bar_x = i * 3;
+    bar_width = 2;
+    bar_height = -(fbc_array[i] / 2);
+    //  fillRect( x, y, width, height ) // Explanation of the parameters below
+    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+  }
+}*/
+
 
 
 
